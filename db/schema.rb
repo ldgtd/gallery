8f5afb7380392e65a04612aca8db8346a5057ac9
Ludovic Turmel <ludovic.turmel@liquid-concept.ch>
2012-04-26 14:21:50 +0200

    add image_store model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120426112229) do
+ActiveRecord::Schema.define(:version => 20120426120156) do
+
+  create_table "image_stores", :force => true do |t|
+    t.text     "name"
+    t.string   "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "images", :force => true do |t|
     t.text     "description"
