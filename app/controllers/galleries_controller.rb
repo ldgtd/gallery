8978351704b8e2b3907d8c8d84e77a7ef9a3f6b6
Ludovic Turmel <ludovic.turmel@liquid-concept.ch>
2012-04-27 17:11:41 +0200

    add show method and view for ImageStore data

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,7 +1,15 @@
 class GalleriesController < ApplicationController
-  def index
+  def index 
+
   end
 
   def show
+    @image_store = ImageStore.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @image_store }
+    end
   end
+
 end
